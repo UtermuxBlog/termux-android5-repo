b2ac9378bbb92d96e6a095110f158ea79c5dfd68
2096779623 <57583560+2096779623@users.noreply.github.com>
2021-10-16 23:11:04 +0800

    Add files via upload

---
+++ 安卓5~6 老版本可用源安装脚本.sh
@@ -0,0 +1,17 @@
+#/data/data/com.termux/files/usr/bin/bash
+#By 2096779623
+rm $PREFIX/etc/apt/sources.list.d/*&>/dev/null 2>&1
+echo 安装git...
+pkg install git -y &>/dev/null
+if [ ! -d "/data/" ];then
+	  echo 正在从fastgit.org下载源... && git clone https://hub.fastgit.org/2096779623/termux-android5-repo &>/dev/null && cd termux-android5-repo 
+	    else
+		      cd termux-android5-repo
+fi
+cp -r * $PREFIX/etc/apt
+if [ `id -u` -ne 0 ]; then
+	rm $PREFIX/etc/apt/sources.list.d/root.list 2>&1
+fi
+echo 删除残留文件... && rm -rf ~/termux-android5-repo&>/dev/null 2>&1 
+
+echo OK!
