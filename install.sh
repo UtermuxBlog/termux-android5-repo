7d9cbf2682415577f7431dcb253e9d48dfdf0d62
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-01-22 12:56:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 rm $PREFIX/etc/apt/sources.list.d/* &>/dev/null 2>&1
 echo 安装git...
 pkg i git -y &>/dev/null
-	  echo 正在从镜像站下载源... && git clone https://github.2096779623.workers.dev/2096779623/termux-android5-repo &>/dev/null && cd termux-android5-repo 
+mkdir -p termux-android5-repo
+cd termux-android5-repo
+echo 正在从镜像站下载源... && git clone https://github.2096779623.workers.dev/2096779623/termux-android5-repo &>/dev/null
 cp -r * $PREFIX/etc/apt
 if [ `id -u` -ne 0 ]; then
 	rm $PREFIX/etc/apt/sources.list.d/root.list 2>&1
