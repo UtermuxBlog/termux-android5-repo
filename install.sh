d94226ce330cc11ea46ca9cc4084f1edf71c444a
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-05-29 11:23:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #/data/data/com.termux/files/usr/bin/bash
 #By 2096779623
 rm $PREFIX/etc/apt/sources.list.d/* &>/dev/null 2>&1
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://packages-cf.termux.org/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list
-echo deb https://packages-cf.termux.org/apt/termux-main stable main > $PREFIX/etc/apt/sources.list
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://packages-cf.termux.org/termux-main-21 stable main@' $PREFIX/etc/apt/sources.list
+echo deb https://packages-cf.termux.org/termux-main-21 stable main > $PREFIX/etc/apt/sources.list
 echo deb https://termux.com/game-packages-21-bin games stable > $PREFIX/etc/apt/sources.list.d/game.list
 echo deb https://termux.com/science-packages-21-bin science stable > $PREFIX/etc/apt/sources.list.d/science.list
-echo deb https://grimler.se/unstable-packages/ unstable main > $PREFIX/etc/apt/sources.list.d/unstable.list
-echo deb http://grimler.se/apt/termux-x11/ x11 main > $PREFIX/etc/apt/sources.list.d/x11.list
 echo deb https://termux.com/termux-root-packages-21-bin root stable > $PREFIX/etc/apt/sources.list.d/root.list
 if [ `id -u` -ne 0 ]; then
 	rm $PREFIX/etc/apt/sources.list.d/root.list 2>&1
